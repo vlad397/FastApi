2de8ad8d441fa04177e697c283eb37de3c592033
Shamil K <hsh0@ya.ru>
2022-04-23 23:11:14 +0300

    genre add index

--- fastpapi-solution/src/models/genre.py
+++ fastpapi-solution/src/models/genre.py
@@ -1,7 +1,11 @@
+from typing import ClassVar
+
 from models.base import BaseMovie
 
 
 class Genre(BaseMovie):
     """Модель жанров"""
+    index: ClassVar[str] = 'genres'
+
     uuid: str   # todo rename
     name: str
